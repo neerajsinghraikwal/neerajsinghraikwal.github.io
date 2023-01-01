625486acd810df31cd0bb064924f77ca307fd8ae
Neeraj Singh <nsrneerajsingh2721@gmail.com>
2023-01-01 17:11:41 +0530

    added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,17 +12,25 @@ const projects = [
     imgPath: "https://user-images.githubusercontent.com/103197193/208367218-a6629c26-0fee-4e39-ba3f-d168af06df15.png",
     title: "Glamour",
     description: "Glamour is an Indian e-commerce company that sells cosmetics and personal care products",
-    link: "https://github.com/sranbir392/Project_1/tree/main/finalproject",
-    demo: "https://jovial-entremet-naturebasketclone.netlify.app/index.html",
-    stack: ["javascript", "node", "html", "css", "mongodb","express"]
+    link: "https://github.com/neerajsinghraikwal/Glamour",
+    demo: "https://glamour-ashy.vercel.app/",
+    stack: ["javascript", "node", "css", "mongodb","express","react","redux"]
   },
   {
     imgPath: "https://user-images.githubusercontent.com/101583807/204452594-f64b42f8-03d4-45f7-aa70-242e474f649f.png",
-    title: "Olx Clone",
-    description: "Becho.com is MERN Stack e-commerce web application that allows for sale from cars, furniture, electronics to jobs and services listings. Buy or sell something today visit the products listing page and you will see all the products, apply filters as per your need and in just a few clicks you can buy any products from the website.",
+    title: "Becho.com",
+    description: "Becho.com is free classifieds and online marketplace that helps users to sell, buy, rent, or discover anything across India.",
     link: "https://github.com/zuhedshaikh95/awesome-volleyball-5374",
     demo: "https://marvelous-pudding-625465.netlify.app/?limit=4",
-    stack: ["javascript", "node", "html", "css", "git","mongodb","express"]
+    stack: ["javascript", "node", "css", "mongodb","express","react","redux"]
+  },
+  {
+    imgPath: zoomcar,
+    title: "Zoomcar",
+    description: "Zoomcar is an Indian car sharing platform, allows you to hire a rental or self-drive car of your choice.",
+    link: "https://github.com/Ranjanji/doted-branch-8352",
+    demo: "https://prayagp8.github.io/Project-zoom-car/",
+    stack: ["javascript", "node", "html", "css", "git"]
   },
   {
     imgPath: nature,
@@ -31,15 +39,7 @@ const projects = [
     link: "https://github.com/sranbir392/Project_1/tree/main/finalproject",
     demo: "https://jovial-entremet-naturebasketclone.netlify.app/index.html",
     stack: ["javascript", "node", "html", "css", "git"]
-  },
-  {
-    imgPath: zoomcar,
-    title: "Zoomcar",
-    description: "Zoomcar is an Indian car sharing platform, headquartered in Banglore",
-    link: "https://github.com/Ranjanji/doted-branch-8352",
-    demo: "https://prayagp8.github.io/Project-zoom-car/",
-    stack: ["javascript", "node", "html", "css", "git"]
-  },
+  }
 ];
 
 
